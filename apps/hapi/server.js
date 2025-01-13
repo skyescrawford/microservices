e0311cf5d50fca60bc77b1ff5a994c728a1bedb6
Aziz <aziz_inc@apps.ipb.ac.id>
2025-01-13 14:59:50 +0700

    change docker image and compose config

--- apps/hapi/server.js
+++ apps/hapi/server.js
@@ -4,8 +4,8 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 4000,
-    host: 'localhost',
+    port: 3002,
+    host: '0.0.0.0',
   });
 
   server.route({
